6a57f2d5c07b65bfd8b6e822829542d631f327e2
mythily-muthu <mythilimuthu18@gmail.com>
2023-01-10 13:48:10 +0530

    Feat: Added the components

---
+++ src/components/AddContact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const AddContact = () => {
+    return (
+        <div className='ui main'>
+            <h2>Add Contact</h2>
+            <div className='ui form'>
+                <div className='field'>
+                    <label>Name</label>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AddContact 
---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import AddContact from './AddContact';
+import './App.css';
+import ContactList from './ContactList';
+import Header from "./Header"
+
+
+
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <AddContact />
+
+
+    </div>
+  );
+}
+
+export default App;
